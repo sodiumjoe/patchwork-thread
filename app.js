5eb3983df9197b9c29e54aa1e74597dd7bc656aa
Joe <joe@appfog.com>
2012-08-14 00:40:35 -0700

    remove vmcpush

--- app.js
+++ app.js
@@ -7,11 +7,6 @@ var exec = require('child_process').exec;
 var assert = require('assert');
 var vmcjs = require('vmcjs');
 
-var target = (process.env.CF_TARGET);
-var user = (process.env.CF_USER);
-var pwd = (process.env.CF_PWD);
-var whitelist = (process.env.CF_WHITELIST);
-
 var fs = require('fs');
 
 function puts(error, stdout, stderr) { util.puts(stdout) }
@@ -68,7 +63,6 @@ app.post('/pusher', function(req, res){
 		if (err) throw err;
 		else {
 		    console.log('successful clone');
-		    vmcPush(obj.repository.name)
 		} 
 	    });
 
@@ -93,33 +87,3 @@ app.get('/', function(req, res) {
 
 
 app.listen(process.env.VCAP_APP_PORT || 3000);
-
-
-
-
-function vmcPush(appName) {
-
-    console.log('trying the push: ' + appName);
-    var vmc = new vmcjs.VMC(target, user, pwd);
-    console.log('kicked off the login');
-
-    vmc.login(function(err) {
-       console.log('successful login');	    
-       assert.equal(err, undefined, "Unexpected err in login: " + util.inspect(err));
-       var appDir = './fixtures/' + appName;
-
-
-    // delete our test app if already exists (purposely ignore any errors)
-    vmc.deleteApp(appName, function(err, data){
-        vmc.push(appName, appDir, function(err) {
-            assert.equal(err, undefined, "Unexpected err in push: " + util.inspect(err));
-	    vmc.start(appName, function(err, data){
-		    vmc.apps(function(err, apps) {
-			    console.log("done successfully starting app");
-			});
-		});
-            });
-        });
-    });
-
-}

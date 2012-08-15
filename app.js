164733dbfbc672ceae2fad60631225e62c6183a2
Joe Moon <joe@appfog.com>
2012-08-14 17:22:52 -0700

    mongodb

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 var express = require('express');
 var app = express.createServer();
 app.use(express.logger());
+var mongo;
+app.configure('development', function(){
+    mongo = {
+        "hostname":"localhost",
+        "port":27017,
+        "username":"",
+        "password":"",
+        "name":"",
+        "db":"db"
+    }
+});
+app.configure('production', function(){
+    var env = JSON.parse(process.env.VCAP_SERVICES);
+    mongo = env['mongodb-1.8'][0]['credentials'];
+});
+var generate_mongo_url = function(obj){
+    obj.hostname = (obj.hostname || 'localhost');
+    obj.port = (obj.port || 27017);
+    obj.db = (obj.db || 'test');
+    if(obj.username && obj.password){
+        return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+    }else{
+        return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+    }
+}
+var mongourl = generate_mongo_url(mongo);
 var util = require('util')
 var exec = require('child_process').exec;
 
@@ -95,7 +121,28 @@ app.get('/pusher', function(req, res){
 
 app.get('/', function(req, res) {
 	res.send('Hello from Cloud Foundry');
-    });
+});
+
+app.get('/mongo', function(req, res) {
+    record_visit(req, res);
+});
 
 
 app.listen(process.env.VCAP_APP_PORT || 3000);
+
+var record_visit = function(req, res){
+    /* Connect to the DB and auth */
+    require('mongodb').connect(mongourl, function(err, conn){
+        conn.collection('ips', function(err, coll){
+            /* Simple object to insert: ip address and date */
+            object_to_insert = { 'ip': req.connection.remoteAddress, 'ts': new Date() };
+            /* Insert the object then print in response */
+            /* Note the _id has been created */
+            coll.insert( object_to_insert, {safe:true}, function(err){
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(JSON.stringify(object_to_insert));
+            res.end('\n');
+            });
+        });
+    });
+}

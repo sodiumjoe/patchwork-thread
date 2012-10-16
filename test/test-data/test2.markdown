d46a9ecbb797f6c229cef0e7f0ee6f4f435ec2d9
Joe Moon <joe@appfog.com>
2012-10-16 12:05:40 -0700

    adding test data

---
+++ test/test-data/test2.markdown
@@ -0,0 +1,6 @@
+---
+title: Test Number Two
+weight: 1
+---
+
+This is the second test file.
--- test/test.parseContent.js
+++ test/test.parseContent.js
@@ -10,9 +10,27 @@ suite('parseContent', function() {
     app.parseContent('test/test-data/test1.markdown', ghrepo, 'joebadmo/indexer', function(err, parsedObj) {
 		if(err){
 			console.log(err);
-		}else{
-			assert.equal(parsedObj.title, 'Test', 'Title does not match: ' + parsedObj.title);
 		}
+			assert.equal(parsedObj.title, 'Test', 'Title does not match: ' + parsedObj.title);
+			assert.equal(parsedObj.weight, 0, 'Weight does not match: ' + parsedObj.weight);
+			assert.equal(parsedObj.content, "This is the first test file.\n", 'Content does not match: ' + parsedObj.content);
+			assert.equal(parsedObj.docid, "test-test-data-test1", 'DocID does not match: ' + parsedObj.docid);
+			assert.equal(parsedObj.path, "test/test-data/test1", 'Path does not match: ' + parsedObj.path);
+			assert.equal(parsedObj.category, "test.test-data", 'Category does not match: ' + parsedObj.category);
+      done();
+    });
+  });
+  test('parseContent on test2.markdown should parse the content', function(done) {
+    app.parseContent('test/test-data/test2.markdown', ghrepo, 'joebadmo/indexer', function(err, parsedObj) {
+		if(err){
+			console.log(err);
+		}
+			assert.equal(parsedObj.title, 'Test Number Two', 'Title does not match: ' + parsedObj.title);
+			assert.equal(parsedObj.weight, 1, 'Weight does not match: ' + parsedObj.weight);
+			assert.equal(parsedObj.content, "This is the second test file.\n", 'Content does not match: ' + parsedObj.content);
+			assert.equal(parsedObj.docid, "test-test-data-test2", 'DocID does not match: ' + parsedObj.docid);
+			assert.equal(parsedObj.path, "test/test-data/test2", 'Path does not match: ' + parsedObj.path);
+			assert.equal(parsedObj.category, "test.test-data", 'Category does not match: ' + parsedObj.category);
       done();
     });
   });

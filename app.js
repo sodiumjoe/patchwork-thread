4df7c0a042fcee29c87b767c90d6c00373d891ba
Joe Moon <joe@appfog.com>
2012-10-17 14:29:03 -0700

    moved search functions to lib/search.js

--- app.js
+++ app.js
@@ -184,29 +184,6 @@ app.get('/index/:conf', function(req, res){
     }
 });
 
-function indexToSearch(fileObj, searchifyIndex, searchifyClient, callback){
-    searchifyClient.put('/v1/indexes/' + searchifyIndex + '/docs', {docid: fileObj.docid, fields: {text: fileObj.content, title: fileObj.title, path: fileObj.path}}, function(err, req, res, obj){
-        if(err){
-            callback(err);
-        }else{
-            console.log("Indexed to searchify: " + fileObj.path);
-            callback(null);
-        }
-    });
-}
-
-function deindexFromSearch(path, searchifyIndex, searchifyClient, callback){
-    var delPath = '/v1/indexes/' + searchifyIndex + '/docs/?' + 'docid=' + path.replace('.markdown','').replace('.md','');
-    searchifyClient.del(delPath, function(err, req, res){
-        if(err){
-            callback(err);
-        }else{
-            console.log('deindexed from searchify: ' + path);
-            callback(null);
-        }
-    });
-}
-
 function addToDB (fileObj, mongoDoc, callback) {
     mongoDoc.findOne({'path': fileObj.path}, function(err, doc){
         if(err){
@@ -398,8 +375,6 @@ function saveMenu(menuArr, currentConf, mongoMenu, callback){
     });
 }
 
-exports.indexToSearch = indexToSearch;
-exports.deindexFromSearch = deindexFromSearch;
 exports.addToDB = addToDB;
 exports.removeFromDB = removeFromDB;
 exports.indexMenu = indexMenu;

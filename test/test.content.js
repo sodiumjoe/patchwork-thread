5470a488274ab4ca47f2b39416f777da830598cb
Joe <joe@appfog.com>
2012-10-20 11:52:43 -0700

    optionally use Github creds in env var to use basic auth for higher api rate limit

--- test/test.content.js
+++ test/test.content.js
@@ -1,8 +1,8 @@
 var content = require('../lib/content');
-//request = require('request'),
 var path = '/',
     fileConf = {
-        ghrepo: {
+        github: {
+            ghrepo: {
                 contents: function(path, callback){
                     var data = {
                         type: 'file',
@@ -10,20 +10,17 @@ var path = '/',
                     };
                     callback(null, data);
                 }
-        },
-        github: {
-            repo: 'fake'
+            }
         }
     };
 
-
-    exports['test getContent'] = function (test) {
-        global.request = function(options, callback){
-            callback(null, 'raw content', 'body');
-        };
-
-        content.getContent(path, fileConf, function(err, rawContent){
-            test.equal(rawContent, 'raw content');
-            test.done();
-        });
+exports['test getContent'] = function (test) {
+    request = function(options, callback){
+        callback(null, 'raw content', 'body');
     };
+
+    content.getContent(path, fileConf, function(err, rawContent){
+        test.equal(rawContent, 'raw content');
+        test.done();
+    });
+};

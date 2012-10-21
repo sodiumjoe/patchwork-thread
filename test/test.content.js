1c555448c9c1aabb3d801a08bf125afa95452c3a
Joe <joe@appfog.com>
2012-10-21 16:57:10 -0700

    added test for parseDir

--- test/test.content.js
+++ test/test.content.js
@@ -1,43 +1,91 @@
+// Test override function
 var fakeRequest = function(options, callback){
-    var rawContent = {
-        body: 'raw content'
-    };
-    callback(null, rawContent, 'body');
+    callback(null, { body: 'raw content' }, 'body');
 };
 
+// Library to test
 var content = require('../lib/content')(fakeRequest);
-var path = '/',
-    fileConf = {
-        github: {
-            ghrepo: {
-                contents: function(path, callback){
-                    var data = {
-                        type: 'file'
-                    };
-                    callback(null, data);
-                }
-            }
-        }
-    };
 
 exports['test getContent'] = function (test) {
-    content.getContent(path, fileConf, function(err, rawContent){
+    // test data
+    var path = '/',
+        conf = {
+            github: {
+                ghrepo: {
+                    contents: function(path, callback){
+                        var data = {
+                            type: 'file'
+                        };
+                        callback(null, data);
+                    }
+                }
+            }
+        };
+
+    test.expect(1);
+    content.getContent(path, conf, function(err, rawContent){
         test.equal(rawContent, 'raw content');
         test.done();
     });
 };
 
-var goodYamlFront = '---\ntitle: "Test Title 1"\n---';
-var badYamlFront = 'things';
-
 exports['test parseContent'] = function (test) {
-    test.expect(2);
+    // test data
+    var goodYamlFront = '---\ntitle: "Test Title 1"\nweight: 0\narbitrary: things\n---\n\nHello this is the content.',
+        badYamlFront = 'things';
+
+    test.expect(6);
     content.parseContent(goodYamlFront, function(err, parsedObj){
         test.equal(parsedObj.title, 'Test Title 1');
+        test.equal(parsedObj.weight, 0);
+        test.equal(parsedObj.arbitrary, 'things');
+        test.equal(parsedObj.content, 'Hello this is the content.');
+
         content.parseContent(badYamlFront, function(err, parsedObj){
             test.equal(typeof parsedObj, 'undefined');
+            test.equal(err, "Error parsing yaml front matter because of no match in file: ");
             test.done();
         });
     });
+};
 
+exports['test parseDir'] = function (test) {
+    // test data
+    var path = '',
+        dataArray = [
+            {
+                path: 'test1.markdown',
+                type: 'file'
+            },
+            {
+                path: 'test2.markdown',
+                type: 'file'
+            },
+            {
+                path: 'test3.md',
+                type: 'file'
+            }
+        ],
+        conf = {
+            github: {
+                ghrepo: {
+                    contents: function(p, callback){
+                        callback(null, dataArray);
+                    }
+                }
+            }
+        },
+        testContainer = [],
+        fileFunc = function(p, callback){
+            testContainer.push(p);
+            callback(null);
+        };
+
+    test.expect(3);
+    content.parseDir(path, conf, fileFunc, function(err, rawContent){
+        test.equal(testContainer[0], 'test1.markdown');
+        test.equal(testContainer[1], 'test2.markdown');
+        test.equal(testContainer[2], 'test3.md');
+        test.done();
+    });
 };

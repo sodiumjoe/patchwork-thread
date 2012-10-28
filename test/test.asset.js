0bfccce3219195871881092bb225ee861dc3fe46
Joe <joe@appfog.com>
2012-10-28 01:18:34 -0700

    added more tests to asset.getAssetList

--- test/test.asset.js
+++ test/test.asset.js
@@ -41,11 +41,16 @@ var dir = [
     };
 
 exports['test asset.getAssetList'] = function (test) {
+    test.expect(5);
     asset.getAssetList('whatever', conf, function(err, assetArray){
         if(err){
             console.log(err);
         }else{
             test.equals(assetArray.length, 4);
+            test.equals(assetArray[0].path, 'asset1.png');
+            test.equals(assetArray[1].path, 'asset2.png');
+            test.equals(assetArray[2].path, 'asset3.png');
+            test.equals(assetArray[3].path, 'assetDir/asset4.png');
             test.done();
         }
     });

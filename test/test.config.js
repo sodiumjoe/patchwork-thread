c41636341b6aa8a1fb775a93f1700746f14dde8d
Joe <joe@appfog.com>
2013-05-18 02:16:13 -0700

    config tests

--- test/test.config.js
+++ test/test.config.js
@@ -1,248 +1,93 @@
-var fakeEnvironment = {
-        GITHUB_PASSWORD: 'Something',
-        SEARCHIFY_PRIVATE_API_URL: 'Something2',
-        FAKE_SEARCHIFY_ENV_VAR: 'Something3'
-    },
-    fakeEnvironment2 = {
-        GITHUB_PASSWORD: 'Something'
-    },
-    fakeRestify = {
-        createJsonClient: function(trash){
-            return {
-                get: function(dummy, callback){
-                    var response = {
-                        statusCode: 200
-                    };
-                    callback(null, null, response, null);
-                }
-            }
-        }
-    },
-    fakeMongoose = {
-        createConnection: function(url, db){
-            return {
-                on: function(dummy1, dummy2){
-                    return;
-                },
-                model: function(modelName, schema){
-                    return modelName;
-                }
-            }
-        }
-    },
-    fakeKnox = {
-        createClient: function(obj){
-            return obj;
-        }
-    },
-    github = require('octonode'),
-    client = github.client({
-        username: 'joebadmo',
-        password: 'Something'
-    }),
-    repo = client.repo('joebadmo/patchwork'),
-    fakeConf = {
-    github: {
-        user: 'joebadmo',
-        repo: 'patchwork'
-    },
-    db: 'explicitDB'
-};
+var should = require('should')
+  , assert = require('assert')
+  ;
 
-// Libarary to test
-var config = require('../lib/config')('./test/test-data/test.config.yml', fakeEnvironment, fakeRestify, fakeMongoose);
+describe( 'lib/config.js', function(){
+    var config, confPath;
 
-exports['test getConf'] = function(test){
-    test.expect(5);
-    var testConf = config.getConf('joebadmo', 'patchwork');
-    test.equal(testConf.github.user, 'joebadmo');
-    test.equal(testConf.github.repo, 'patchwork');
-    test.equal(testConf.blog.path, 'blog');
-    test.equal(testConf.assets.path, 'assets');
+    confPath = '../lib/config';
 
-    var failConf = config.getConf('fake', 'shouldFail');
-    test.equal(failConf, null);
-    test.done();
-};
+    describe( 'github client configuration', function(){
+        describe( 'should create github client properly', function(){
+            var opts = {}, mockClient, mockConfig, ghclient; 
 
-exports['test configureGitHub'] = function (test) {
-    config.configureGitHub(fakeConf);
-    test.expect(6);
-    test.equal(fakeConf.github.credentials.username, 'joebadmo');
-    test.equal(fakeConf.github.credentials.password, 'Something');
-    test.equal(fakeConf.github.ghrepo.name, client.repo('joebadmo/patchwork').name);
-    test.equal(fakeConf.github.ghrepo.repo, client.repo('joebadmo/patchwork').repo);
-    test.equal(fakeConf.github.ghrepo.credentials, client.repo('joebadmo/patchwork').credentials);
-    test.equal(fakeConf.github.repoName, 'joebadmo/patchwork');
-    test.done();
-};
+            mockClient = function( obj ) {
+                var client = obj || {};
+                client.repo = function(){};
+                return client;
+            };
 
-exports['test configureSearchify'] = {
-    'no searchify': function(test){
-        config = require('../lib/config')('./test/test-data/test.config.yml', fakeEnvironment2, fakeRestify);
-        config.configureSearchify(fakeConf);
-        test.equal(fakeConf.searchify.url, null);
-        test.equal(fakeConf.searchify.index, 'joebadmo-patchwork');
-        test.done();
-    },
-    'default env var': function(test){
-        config = require('../lib/config')('./test/test-data/test.config.yml', fakeEnvironment, fakeRestify);
-        config.configureSearchify(fakeConf);
-        test.equal(fakeConf.searchify.url, 'Something2');
-        test.equal(fakeConf.searchify.index, 'joebadmo-patchwork');
-        test.done();
-    },
-    'explicit env var': function(test){
-        fakeConf.searchify.private_api_env_var = 'FAKE_SEARCHIFY_ENV_VAR';
-        config.configureSearchify(fakeConf);
-        test.equal(fakeConf.searchify.url, 'Something3');
-        test.equal(fakeConf.searchify.index, 'joebadmo-patchwork');
-        test.done();
-    }, 
-    'explicit index': function(test){
-        fakeConf.searchify.index = 'fakeIndex';
-        config.configureSearchify(fakeConf);
-        test.equal(fakeConf.searchify.index, 'fakeIndex');
-        test.done();
-    }
-};
+            opts.github = { client: mockClient
+                          };
+            opts.config = { github:
+                                { user: 'joebadmo'
+                                , repo: 'patchwork'
+                                }
+                          };
 
-exports['test configureDatabase'] = {
-    'explicit db name': function(test){
-        config.configureDatabase(fakeConf);
-        test.equal(fakeConf.db, 'explicitDB');
-        test.done();
-    },
-    'no db in conf': function(test){
-        fakeConf.db = null;
-        config.configureDatabase(fakeConf);
-        test.equal(fakeConf.db, 'joebadmo-patchwork');
-        test.done();
-    },
-    'local dev env': function(test){
-        config.configureDatabase(fakeConf);
-        test.equal(fakeConf.Doc, 'document');
-        test.equal(fakeConf.Menu, 'menu');
-        test.done();
-    },
-    'no blog': function(test){
-        config.configureDatabase(fakeConf);
-        test.equal(fakeConf.Blog, null);
-        test.done();
-    },
-    'blog': function(test){
-        fakeConf.blog = 'fakeBlog';
-        config.configureDatabase(fakeConf);
-        test.equal(fakeConf.Blog, 'blog');
-        test.done();
-    },
-    'no matching db in VCAP_SERVICES': function(test){
-        var vcap = JSON.stringify({
-            'mongodb-1.8': [
-                {
-                    name: 'fakeDB',
-                    credentials: {
-                        url: null
-                    }
-                },
-                {
-                    name: 'joebadmo-patchwork',
-                    credentials: {
-                        url: 'testURL'
-                    }
-                }
-            ]
+            it( 'should use given credentials for basic auth', function(){
+                mockConfig = { github:
+                                   { credentials: 
+                                         { username: 'badmo'
+                                         , password: 'Password'
+                                         }
+                                   , user: 'joe'
+                                   , repo: 'patchwork'
+                                   }
+                             };
+
+                config = require( confPath )( opts );
+                ghclient = config.getGithubClient( mockConfig );
+                ghclient.username.should.equal( 'badmo' );
+                ghclient.password.should.equal( 'Password' );
+            });
+
+            it( 'should use github user if no username is specified', function(){
+                mockConfig = { github:
+                                   { credentials: 
+                                         { password: 'Password'
+                                         }
+                                   , user: 'joe'
+                                   , repo: 'patchwork'
+                                   }
+                             };
+
+                config = require( confPath )( opts );
+                ghclient = config.getGithubClient( mockConfig );
+                ghclient.username.should.equal( 'joe' );
+                ghclient.password.should.equal( 'Password' );
+            });
+
+            it( 'should create client without auth if no password is supplied', function(){
+                mockConfig = { github:
+                                   { credentials: 
+                                         { username: 'joebadmo'
+                                         }
+                                   , user: 'joe'
+                                   , repo: 'patchwork'
+                                   }
+                             };
+
+                config = require( confPath )( opts );
+                ghclient = config.getGithubClient( mockConfig );
+                should.not.exist( ghclient.username );
+                should.not.exist( ghclient.password );
+            });
         });
+    });
 
-        fakeEnvironment.VCAP_SERVICES = vcap;
-        fakeConf.db = 'no-match';
-        config = require('../lib/config')('./test/test-data/test.config.yml', fakeEnvironment, fakeRestify, fakeMongoose);
-        config.configureDatabase(fakeConf);
-        test.equal(fakeConf.mongoConnectionURI, 'localhost');
-        test.equal(fakeConf.Doc, 'document');
-        test.equal(fakeConf.Menu, 'menu');
-        test.done();
-    },
-    'matching db in VCAP_SERVICES': function(test){
-        fakeConf.db = null;
-        config.configureDatabase(fakeConf);
-        test.equal(fakeConf.mongoConnectionURI, 'testURL');
-        test.equal(fakeConf.Doc, 'document');
-        test.equal(fakeConf.Menu, 'menu');
-        test.done();
-    }
-};
+    describe( 'get()', function(){
+        config = require( confPath )({ 
+            config: { github:
+                          { user: 'joebadmo'
+                          , repo: 'patchwork'
+                          }
+                    }
+        });
+        it( 'should return the configuration object for the given user and repo', function(){
+            var conf = config.get( 'joebadmo', 'patchwork' );
+            conf.should.be.a('object');
+        })
+    })
 
-exports['test configureS3'] = {
-    'no assets': function(test){
-        config.configureS3(fakeConf);
-        test.equal(fakeConf.assets, null);
-        test.done();
-    },
-    'just assets path': function(test){
-        fakeConf.assets = 'fakeAssets';
-        config.configureS3(fakeConf);
-        test.equal(fakeConf.assets.path, 'fakeAssets');
-        test.equal(fakeConf.assets.S3, null);
-        test.done();
-    },
-    'no key and secret': function(test){
-        config.configureS3(fakeConf);
-        test.equal(fakeConf.assets.S3, null);
-        test.done();
-    },
-    'default key and secret': function(test){
-        fakeEnvironment.S3_ACCESS_KEY = 'fakeKey';
-        fakeEnvironment.S3_SECRET = 'fakeSecret';
-        config = require('../lib/config')('./test/test-data/test.config.yml', fakeEnvironment, fakeRestify, fakeMongoose, fakeKnox);
-        config.configureS3(fakeConf);
-        test.equal(fakeConf.assets.S3.key, 'fakeKey');
-        test.equal(fakeConf.assets.S3.secret, 'fakeSecret');
-        test.equal(fakeConf.assets.S3.client.key, 'fakeKey');
-        test.equal(fakeConf.assets.S3.client.secret, 'fakeSecret');
-        test.equal(fakeConf.assets.S3.client.bucket, 'joebadmo-patchwork');
-        test.done();
-    },
-    'explicit key and secret': function(test){
-        fakeEnvironment.CUSTOM_S3_ACCESS_KEY = 'customFakeKey';
-        fakeEnvironment.CUSTOM_S3_SECRET = 'customFakeSecret';
-        fakeConf.assets.S3.access_key_env_var = 'CUSTOM_S3_ACCESS_KEY';
-        fakeConf.assets.S3.secret_env_var = 'CUSTOM_S3_SECRET';
-        config = require('../lib/config')('./test/test-data/test.config.yml', fakeEnvironment, fakeRestify, fakeMongoose, fakeKnox);
-        config.configureS3(fakeConf);
-        test.equal(fakeConf.assets.S3.key, 'customFakeKey');
-        test.equal(fakeConf.assets.S3.secret, 'customFakeSecret');
-        test.equal(fakeConf.assets.S3.client.key, 'customFakeKey');
-        test.equal(fakeConf.assets.S3.client.secret, 'customFakeSecret');
-        test.equal(fakeConf.assets.S3.client.bucket, 'joebadmo-patchwork');
-        test.done();
-    },
-    'default bucket': function(test){
-        config.configureS3(fakeConf);
-        test.equal(fakeConf.assets.S3.bucket, 'joebadmo-patchwork');
-        test.equal(fakeConf.assets.S3.client.bucket, 'joebadmo-patchwork');
-        test.done();
-    },
-    'explicit bucket': function(test){
-        fakeConf.assets.S3.bucket = 'fakebucket';
-        config.configureS3(fakeConf);
-        test.equal(fakeConf.assets.S3.bucket, 'fakebucket');
-        test.equal(fakeConf.assets.S3.client.bucket, 'fakebucket');
-        test.done();
-    }
-};
-
-exports['test configureBlog'] = {
-    'blog': function(test){
-        fakeConf.blog = 'fakeBlogPath';
-        config.configureBlog(fakeConf);
-        test.equal(fakeConf.blog.path, 'fakeBlogPath');
-        test.done();
-    },
-    'no blog': function(test){
-        fakeConf.blog = null;
-        config.configureBlog(fakeConf);
-        test.equal(fakeConf.blog, null);
-        test.done();
-    }
-};
+})
